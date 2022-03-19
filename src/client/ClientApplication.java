847051b3a6405cb34bf532bb8e6e13058bb99ad2
Dmitriy G <dmitry.gorokhovtsev@robusta.team>
2022-03-19 03:31:11 +0300

    Implement nio

--- src/client/ClientApplication.java
+++ src/client/ClientApplication.java
@@ -1,88 +1,67 @@
 package client;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.Arrays;
-import java.util.List;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.Scanner;
-import java.util.UUID;
 
 public class ClientApplication {
     public static void main(String[] args) {
         String serverName = args[0];
         int port = Integer.parseInt(args[1]);
-        String clientGuid = UUID.randomUUID().toString();
         try {
-            System.out.println("Connect to " + serverName + " port " + port);
-            Socket client = new Socket(serverName, port);
-
-            System.out.println("Connect to " + client.getRemoteSocketAddress());
-            OutputStream outToServer = client.getOutputStream();
-            InputStream inFromServer = client.getInputStream();
-            DataOutputStream out = new DataOutputStream(outToServer);
-            DataInputStream in = new DataInputStream(inFromServer);
-            register(out, clientGuid);
+            SocketChannel channel = SocketChannel.open();
+            Selector selector = Selector.open();
+            channel.configureBlocking(false);
+            channel.connect(new InetSocketAddress(serverName, port));
+            channel.register(selector, SelectionKey.OP_READ);
+            while (!channel.finishConnect()) {
+                System.out.println("still connecting");
+            }
+            System.out.println("Connect to " + channel.getRemoteAddress());
+            //register(out, clientGuid);
 
             //New thread for async listen event bus (new message from server)
-            new Thread(new InputDataThread(in)).start();
+            //new Thread(new InputDataThread(in)).start();
 
             //Scanner in the loop for receive user commands via command line
             Scanner scanner = new Scanner(System.in);
             while (true) {
-                String[] commandArgs = scanner.nextLine().split(" ");
-                String commandType = commandArgs[0];
-                switch (commandType) {
-                    case "message" -> {
-                        String target = commandArgs[1];
-                        String text = commandArgs[2];
-                        doChat(out, clientGuid, target, text);
-                    }
-                    case "users" -> {
-                        requestUsers(in, out, clientGuid);
-                    }
-                    case "exit" -> {
-                        out.writeUTF("Client " + clientGuid + " disconnected");
-                        client.close();
-                        return;
+                // read command and send it to server
+                String command = scanner.nextLine();
+                CharBuffer  c = CharBuffer.wrap(command);
+                ByteBuffer b = StandardCharsets.ISO_8859_1.encode(c);
+                channel.write(b);
+                System.out.println("Command " + command + " was send");
+
+                // read response from server with result of this command
+                // TODO: select() can block! But we need wait server response for testing it
+                // TODO: need listener
+                if (selector.select() == 0) {
+                    continue;
+                }
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
+                    iterator.remove();
+                    if (key.isReadable()) {
+                        ByteBuffer buffer = ByteBuffer.allocate(100);
+                        channel.read(buffer);
+                        buffer.flip();
+                        System.out.println(Charset.defaultCharset().decode(
+                                buffer));
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    //Initial new message event
-    public static void doChat(DataOutputStream outputStream, String userGuid, String destination, String message) throws IOException {
-        outputStream.writeUTF("message|" + userGuid + "|" + destination + "|" + message);
-    }
-
-    //Initial new user event
-    public static void register(DataOutputStream outputStream, String userGuid) throws IOException {
-        System.out.println("Registration event send");
-        outputStream.writeUTF("register|" + userGuid);
-    }
-
-    //Request users list
-    @Deprecated
-    public static List<String> requestUsers(DataInputStream inputStream, DataOutputStream outputStream, String userGuid) throws IOException {
-        System.out.println("Users list was requested");
-        outputStream.writeUTF("users|" + userGuid);
-        String id = "";
-        while (true) {
-            String[] serverResponse = inputStream.readUTF().split("\\|");
-            id = serverResponse[0];
-            if (id.equals("usersList")) {
-                String data = serverResponse[1];
-                System.out.println("User list received " + data);
-                return Arrays.asList(data.split(",").clone());
-            } else {
-                System.out.println("Waiting user list...");
-            }
         }
     }
 }
--- src/server/ServerApplication.java
+++ src/server/ServerApplication.java
@@ -1,37 +1,143 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class ServerApplication {
-    private static final int SERVER_PORT = 50066;
+    private static String clientChannel = "clientChannel";
+    private static String serverChannel = "serverChannel";
+    private static String channelType = "channelType";
 
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-            System.out.println("Waiting client on port " +
-                    serverSocket.getLocalPort() + "...");
-            while (true) {
-                //Server wait for new client connection
-                Socket server = serverSocket.accept();
-                System.out.println("Connect to " + server.getRemoteSocketAddress());
-                DataInputStream in = new DataInputStream(server.getInputStream());
-                DataOutputStream out = new DataOutputStream(server.getOutputStream());
-                String[] clientData = in.readUTF().split("\\|");
-                String newClientGuid = clientData[1];
-                UserStreams streams = new UserStreams(in, out);
-                SpikeStorage.clients.put(newClientGuid, streams);
-                System.out.println("User " + newClientGuid + " was registered");
-                //out.writeUTF("Success connect to " + server.getLocalSocketAddress());
+            String serverName = args[0];
+            int port = Integer.parseInt(args[1]);
+            // create a new serversocketchannel. The channel is unbound.
+            ServerSocketChannel channel = ServerSocketChannel.open();
 
-                //New thread for every user (may be we can optimize it via reuse with ThreadPool?)
-                new Thread(new ClientThread(newClientGuid, streams)).start();
+            // bind the channel to an address. The channel starts listening to
+            // incoming connections.
+            channel.bind(new InetSocketAddress(serverName, port));
+
+            // mark the serversocketchannel as non blocking
+            channel.configureBlocking(false);
+
+            // create a selector that will by used for multiplexing. The selector
+            // registers the socketserverchannel as
+            // well as all socketchannels that are created
+            Selector selector = Selector.open();
+
+            // register the serversocketchannel with the selector. The OP_ACCEPT
+            // option marks
+            // a selection key as ready when the channel accepts a new connection.
+            // When the
+            // socket server accepts a connection this key is added to the list of
+            // selected keys of the selector.
+            // when asked for the selected keys, this key is returned and hence we
+            // know that a new connection has been accepted.
+            SelectionKey socketServerSelectionKey = channel.register(selector,
+                    SelectionKey.OP_ACCEPT);
+
+            Map<String, String> properties = new HashMap<>();
+            properties.put(channelType, serverChannel);
+            socketServerSelectionKey.attach(properties);
+
+            while (true) {
+                // the select method is a blocking method which returns when atleast
+                // one of the registered
+                // channel is selected. In this example, when the socket accepts a
+                // new connection, this method
+                // will return. Once a socketclient is added to the list of
+                // registered channels, then this method
+                // would also return when one of the clients has data to be read or
+                // written. It is also possible to perform a nonblocking select
+                // using the selectNow() function.
+                // We can also specify the maximum time for which a select function
+                // can be blocked using the select(long timeout) function.
+                if (selector.select() == 0)
+                    continue;
+                // the select method returns with a list of selected keys
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectedKeys.iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey key = iterator.next();
+                    // the selection key could either by the socketserver informing
+                    // that a new connection has been made, or
+                    // a socket client that is ready for read/write
+                    // we use the properties object attached to the channel to find
+                    // out the type of channel.
+                    if (((Map<String, String>) key.attachment()).get(channelType).equals(
+                            serverChannel)) {
+                        // a new connection has been obtained. This channel is
+                        // therefore a socket server.
+                        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key
+                                .channel();
+                        // accept the new connection on the server socket. Since the
+                        // server socket channel is marked as non blocking
+                        // this channel will return null if no client is connected.
+                        SocketChannel clientSocketChannel = serverSocketChannel
+                                .accept();
+
+                        if (clientSocketChannel != null) {
+                            // set the client connection to be non blocking
+                            clientSocketChannel.configureBlocking(false);
+                            SelectionKey clientKey = clientSocketChannel.register(
+                                    selector, SelectionKey.OP_READ,
+                                    SelectionKey.OP_WRITE);
+                            Map<String, String> clientProperties = new HashMap<>();
+                            clientProperties.put(channelType, clientChannel);
+                            clientKey.attach(clientProperties);
+                        }
+                    } else {
+                        // data is available for read
+                        // buffer for reading
+                        ByteBuffer buffer = ByteBuffer.allocate(100);
+                        SocketChannel clientChannel = (SocketChannel) key.channel();
+                        int bytesRead;
+                        CharBuffer commandFromClient = null;
+                        if (key.isReadable()) {
+                            // the channel is non blocking so keep it open till the
+                            // count is >=0
+                            if ((bytesRead = clientChannel.read(buffer)) > 0) {
+                                buffer.flip();
+                                commandFromClient = Charset.defaultCharset().decode(
+                                        buffer);
+                                System.out.println(commandFromClient);
+                                buffer.clear();
+                            }
+                            // send result of the command for client
+                            CharBuffer writeBuffer = CharBuffer.wrap("Do you sent this command " + commandFromClient + " ?");
+                            while (writeBuffer.hasRemaining()) {
+                                clientChannel.write(Charset.defaultCharset()
+                                        .encode(writeBuffer));
+                            }
+                            buffer.clear();
+
+
+                            if (bytesRead < 0) {
+                                // the key is automatically invalidated once the
+                                // channel is closed
+                                clientChannel.close();
+                            }
+                        }
+                    }
+
+                    // once a key is handled, it needs to be removed
+                    iterator.remove();
+                }
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
